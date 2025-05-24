5ff9d81c0e6ac90306344014587badfdd437b595
N0lifer <gluhov.m.n@gmail.com>
2025-05-24 10:27:48 +0500

    feat: add CoursesVmBuilder ViewModelBuilder

---
+++ StudyTracker/ViewModelBuilders/CoursesVmBuilder.cs
@@ -0,0 +1,21 @@
+﻿using StudyTracker.Models;
+using StudyTracker.ViewModels;
+
+namespace StudyTracker.ViewModelBuilders
+{
+    public class CoursesVmBuilder
+    {
+        public CoursesVmBuilder() { }
+
+        public CoursesVm GetCoursesVm()
+        {
+            var course1 = new Course(id: 1, "Math", "some description", "Ivanov");
+            var course2 = new Course(id: 2, "History", "world history", "Petrov");
+            var course3 = new Course(id: 3, "Computer Science", description: null, "Sidorov");
+
+            var courses = new List<Course>() { course1, course2, course3 };
+
+            return new CoursesVm(courses);
+        }
+    }
+}
